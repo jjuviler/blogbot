9e47cac44f3a3d8f2cbc1bfedcec498abfa151ba
jjuviler <159360361+jjuviler@users.noreply.github.com>
2026-03-13 00:51:24 -0400

    Delete aeo-checker-main directory

--- aeo-checker-main/public/js/checks/legal.js
+++
@@ -1,8 +0,0 @@
-import { postJSON } from '../utils/net.js';
-
-export async function runLegalReview(text, { signal } = {}) {
-  const res = await postJSON('/legal-review', { text }, { signal });
-  return res;
-}
-
-
